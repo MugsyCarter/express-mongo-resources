f52ccb19067fdfd41f1f0dbd66500cf6dbb8cd18
Mugsy Carter <mugsycarter@gmail.com>
2016-11-03 13:20:37 -0700

    everything working great all 4 tests coming back OK.

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
+const app = require('./lib/app');
+const http = require('http');
 const port = process.env.PORT || 3200;
-const server = require('./lib/server.js'); 
+/*const connection = */ require('./lib/setupMongoose');
 
-server.listen(port, ()=>{
-    console.log('app started on port ', server.address().port);
+const server = http.createServer(app);
+server.listen(port, () => {
+    console.log('server running at', server.address());
 });
+module.exports = server;
--- model/animal.js
+++ model/animal.js
@@ -7,21 +7,14 @@ const schema = new Schema({
 		required: true,
 		unique: true
 	},
-	type: {
+	nutrition: {
 		type: String,
-		default: 'gymnosperm',
 		required: true
 	},
 	genus: {
 		type: String,
 		required: true
-	},
-	percentage: {
-		type: Number, 
-		min: 0, 
-		max: 100,
-		required: true
 	}
 });
 
-module.exports = mongoose.model('Tree', schema);
+module.exports = mongoose.model('Animal', schema);
